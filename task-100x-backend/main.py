4fb77728985072b23ccd7b8f7b31dd0b6abec76d
AzureAD\VigneshUdhayakumar <vignesh.uday94@gmail.com>
2025-07-29 19:11:04 +0530

    Refactor FastAPI imports and update Prisma datasource configuration for clarity

--- task-100x-backend/main.py
+++ task-100x-backend/main.py
@@ -2,7 +2,7 @@ import os
 from contextlib import asynccontextmanager
 
 from dotenv import load_dotenv
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from prisma import Prisma
 
