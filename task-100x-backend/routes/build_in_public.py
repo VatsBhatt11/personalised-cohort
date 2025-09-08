7bf8881480ee653c8bd17944d6870e5fb15aef67
Vatsal Bhatt <vatsalbhatt@Vatsals-laptop.local>
2025-09-08 18:11:45 +0530

    Solved issues

--- task-100x-backend/routes/build_in_public.py
+++ task-100x-backend/routes/build_in_public.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from prisma import Prisma
 from typing import List, Optional
 from datetime import datetime
