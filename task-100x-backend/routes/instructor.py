e8bc394db2980c9c0dc662ef46a798deb1778e01
Vatsal Bhatt <vatsalbhatt@Vatsals-laptop.local>
2025-09-10 00:36:01 +0530

    Latest codebase

--- task-100x-backend/routes/instructor.py
+++ task-100x-backend/routes/instructor.py
@@ -176,7 +176,7 @@ async def fetch_linkedin_posts_sequentially(linkedin_cookie_data: LinkedInCookie
                     },
                     "rawData": False,
                     "urls": urls,
-                    "limitPerSource" : 100
+                    "limitPerSource" : 5
                 }
 
                 apify_api_token = os.environ.get("APIFY_API_TOKEN")
