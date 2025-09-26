9d41bd2f39bc50660fe7713268259f60f8f5b148
Vatsal Bhatt <vatsalbhatt@Vatsals-laptop.local>
2025-09-26 12:35:01 +0530

    Small change

--- task-100x/src/lib/api.ts
+++ task-100x/src/lib/api.ts
@@ -203,8 +203,8 @@ export interface UserData {
   totalComments: number;
 }
 
-const API_BASE_URL = "http://localhost:8000";
-// const API_BASE_URL = "https://one00x-be.onrender.com";
+// const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://one00x-be.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
