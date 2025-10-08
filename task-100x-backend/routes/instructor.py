9787dfc33037e12b275bf63969b9b69750975cfe
Vatsal Bhatt <vatsalbhatt@Vatsals-laptop.local>
2025-10-08 10:56:25 +0530

    Increased limit per source

--- task-100x-backend/routes/instructor.py
+++ task-100x-backend/routes/instructor.py
@@ -98,7 +98,7 @@ async def fetch_linkedin_posts(linkedin_cookie_data: LinkedInCookie, current_use
                     },
                     "rawData": False,
                     "urls": urls,
-                    "limitPerSource" : 100
+                    "limitPerSource" : 120
                 }
 
                 apify_api_token = os.environ.get("APIFY_API_TOKEN")
