a4f256de5ae61916d5c59f8eb1faf9e0119fad09
Vatsal Bhatt <vatsalbhatt@Vatsals-laptop.local>
2025-11-20 14:37:40 +0530

    Modified request list UI

--- task-100x/src/pages/RechargeRequestPage.tsx
+++ task-100x/src/pages/RechargeRequestPage.tsx
@@ -26,7 +26,7 @@ const RechargeRequestPage = () => {
   const [showChatModal, setShowChatModal] = useState<boolean>(false);
   const [currentChatHistory, setCurrentChatHistory] = useState<ChatMessage[]>([]);
 
-  const API_BASE_URL = import.meta.env.NEXT_PUBLIC_PROFILE_SYSTEM_API_BASE_URL || 'https://profile-system-100x.vercel.app';
+  const API_BASE_URL = import.meta.env.NEXT_PUBLIC_PROFILE_SYSTEM_API_BASE_URL || 'https://profile-system.vercel.app';
 
   const fetchRechargeRequests = async () => {
     setIsLoading(true);
